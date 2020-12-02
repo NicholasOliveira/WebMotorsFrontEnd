943bc300a483227aa45161f06d6010962d8d85bc
Nicholas Oliveira <nicholas@atomocom.com.br>
2020-12-01 21:41:19 -0300

    update package.json and coverage

--- webmotorsfront/src/__tests__/App.test.js
+++ webmotorsfront/src/__tests__/App.test.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
-import '@testing-library/jest-dom';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "../App";
+import "@testing-library/jest-dom";
 
-describe('App Component', () => {
-  test('renders app component', () => {
+describe("App Component", () => {
+  test("renders app component", () => {
     const { getByTestId } = render(<App />);
-    const linkElement = getByTestId('app');
+    const linkElement = getByTestId("app");
     expect(linkElement).toBeInTheDocument();
   });
 });
