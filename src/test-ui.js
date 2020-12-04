5e6c0ff996a5ea7d60c9db32e6b38d730c39086f
Nicholas Oliveira <nicholas@atomocom.com.br>
2020-12-03 22:03:37 -0300

    update branch

--- src/test-ui.js
+++ src/test-ui.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://google.com');
-  await page.screenshot({ path: './example.png' });
+  await page.goto('https://uol.com.br');
+  await page.screenshot({ path: './uol.png' });
 
   await browser.close();
 })();
