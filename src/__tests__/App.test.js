470b0ad459de799d76e1a322de9a9884f553122e
Nicholas Oliveira <nicholas@atomocom.com.br>
2020-12-06 19:24:46 -0300

    update coverage

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import 'regenerator-runtime';
-import '@testing-library/jest-dom/extend-expect';
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import App from '../App';
+import React from "react";
+import "regenerator-runtime";
+import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import App from "../App";
 
-describe('App Component', () => {
-  test('renders app component', () => {
+describe("App Component", () => {
+  test("renders app component", () => {
     const { getByTestId } = render(<App />);
-    const linkElement = getByTestId('app');
+    const linkElement = getByTestId("app");
     expect(linkElement).toBeInTheDocument();
   });
 });
