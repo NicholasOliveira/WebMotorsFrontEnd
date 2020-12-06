e6982b7fb11b119ff82a9311ebbabbb7c0b74412
Nicholas Oliveira <nicholas@atomocom.com.br>
2020-12-06 19:38:04 -0300

    update coverage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 import { Container } from './styles';
 import BarSearch from './Components/SearchBar';
-
-Sentry.init({
-  dsn: 'https://dc9c71e4849f4af1824b50efb35fe674@o486613.ingest.sentry.io/5546060',
-  integrations: [
-    new Integrations.BrowserTracing(),
-  ],
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-});
 
 function App() {
   const [makes, setMakes] = useState([]);
