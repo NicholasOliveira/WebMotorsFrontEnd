a0b318ef41d911f9d00fc4984fca55ba9cf9cc3d
grupocesw@gmail.com <nicholas@atomocom.com.br>
2020-12-09 20:40:16 -0300

    push code

--- src/runNextPipe.js
+++
@@ -1,3 +0,0 @@
-const axios = require('axios');
-
-axios.get('https://app.buddy.works/grupocesw/webmotorsfrontend/pipelines/pipeline/296140/trigger-webhook?token=d88b755481658bc6bfddb18ab9d75459ce3b8738a497fc313615ee6e31d05afa02a63763d5ad00a8dd4e0192049e349c');
